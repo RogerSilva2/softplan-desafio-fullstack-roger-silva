154896e6db81d2c9533c0570c5e1c272ba67b1d5
ROGER DA SILVA E SILVA <rogersilva84@gmail.com>
2020-11-17 14:01:06 -0300

    Testes nos repositoties

---
+++ process-manager/src/test/java/br/com/rogersilva/processmanager/repository/EvaluationRepositoryTest.java
@@ -0,0 +1,39 @@
+package br.com.rogersilva.processmanager.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import br.com.rogersilva.processmanager.model.Evaluation;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class EvaluationRepositoryTest {
+
+    @Autowired
+    private EvaluationRepository evaluationRepository;
+
+    @Test
+    public void findByIdEvaluatorId() {
+        Long evaluatorId = 3L;
+
+        Optional<List<Evaluation>> evaluations = evaluationRepository.findByIdEvaluatorId(evaluatorId);
+
+        assertThat(evaluations).isPresent();
+        assertThat(evaluations.get()).isNotEmpty();
+        assertThat(evaluations.get().get(0).getId().getEvaluator().getId()).isEqualTo(evaluatorId);
+    }
+
+    @Test
+    public void findByIdEvaluatorIdNotFound() {
+        Optional<List<Evaluation>> evaluations = evaluationRepository.findByIdEvaluatorId(0L);
+
+        assertThat(evaluations).isNotPresent();
+    }
+}
